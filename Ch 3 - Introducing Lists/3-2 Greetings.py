23900d86828c5f88c94e4b79aaf05075d5b42703
who-is-he <monarch.michael.e@gmail.com>
2017-04-05 22:16:44 -0400

    lesson 2 and 3

---
+++ Ch 3 - Introducing Lists/3-2 Greetings.py
@@ -0,0 +1,2 @@
+names = ['jordan', 'alex', 'bobby', 'jason']
+print(names[0].title()+" is really good!",names[1].title()+" is really good!",names[2].title()+" is really good!",names[3].title()+" is really good!")
